3fdb7ed81470ce82b75345a3ceaf9daab7c036a6
Jacob Goldfarb <jg1119.dev@gmail.com>
2020-05-07 19:56:06 -0400

    Committing

---
+++ src/build/precache-manifest.c29092715d3ade768fa7fb1b5c909544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e8dea089fb7bb467ccd",
+    "url": "/static/css/main.1e971301.chunk.css"
+  },
+  {
+    "revision": "0e8dea089fb7bb467ccd",
+    "url": "/static/js/main.9dd2f0bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3411b17d3915443b45c",
+    "url": "/static/js/2.4a7f71bf.chunk.js"
+  },
+  {
+    "revision": "899c11dd2c67e6644628edb28d52e2b5",
+    "url": "/index.html"
+  }
+];
